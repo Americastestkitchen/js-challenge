32fa77a024a5b2839404f2842903aea398f7dc9a
Harry Scheuerle <harry.scheuerle@americastestkitchen.com>
2021-10-03 17:24:27 -0400

    challenge entry: add Typescript to FeaturedBrewery

--- components/FeaturedBrewery/index.js
+++ components/FeaturedBrewery/index.js
@@ -1,8 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import useSWR from 'swr';
+import { breakpoint } from 'styled-components-breakpoint';
 import { usePosition } from 'use-position';
 
 import BreweryHeroItem, { LoadingBreweryHeroItem } from '../BreweryHeroItem';
+
+const HeroWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
+`;
+
+const LocationError = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-left: 8px;
+  margin-top: 8px;
+  padding: 4px;
+  border-radius: 8px;
+  background-color: ${({ theme, accentColor }) => theme.colors[accentColor]};
+  color: white;
+  font-size: 12px;
+
+  ${breakpoint('md')`
+    font-size: 16px;
+  `}
+`;
 
 const FeaturedBrewery = () => {
   const {
@@ -14,19 +38,23 @@ const FeaturedBrewery = () => {
     error: geoError,
   } = usePosition();
 
-  const { data, error } = useSWR(
-    `https://api.openbrewerydb.org/breweries?by_dist${latitude},${longitude}&per_page=1`,
-  );
+  const { data, error } = useSWR(`https://api.openbrewerydb.org/breweries?by_dist${latitude},${longitude}&per_page=1`);
 
-  if (geoError) return null;
-  if (error || data?.length === 0) return "An error has occurred.";
+  if (error || data?.length === 0) return <span>An error has occurred.</span>;
   if (!data) return <LoadingBreweryHeroItem />;
 
   return (
-    <BreweryHeroItem
-      brewery={data?.[0]}
-    />
+    <HeroWrapper>
+      <BreweryHeroItem
+        brewery={data?.[0]}
+      />
+      {geoError && (
+        <LocationError accentColor="primary">
+          Enable location to see nearby locations.
+        </LocationError>
+      )}
+    </HeroWrapper>
   );
-  };
+};
 
 export default FeaturedBrewery;
